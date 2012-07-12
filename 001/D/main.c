331882cae7823deececabd98e05f03b74628ee6d
n-fujita <n-fujita@daiichi-g.co.jp>
2012-07-12 17:24:36 +0900

    delete comment out.

--- 001/D/main.c
+++ 001/D/main.c
@@ -5,28 +5,11 @@ int main()
 	int n,c,g,b,e,y=0,x;
 	double d=0;
 	scanf("%d\n%d%d",&n,&c,&g);
-	// printf("%d %d %d\n",n,s,g);
+	printf("%d %d %d\n",n,c,g);
 	++n;
 	for(;n-->0;){
 		scanf("%d%d",&b,&e);
-		/*
-		printf("%d:%d-%d:",n,b,e);
-		if(b>g && b>c){
-			x=b;
-		}else if(e<g && e<c){
-			x=e;
-		}else if(n==0){
-			x=g;
-		}else{
-			++y;
-			printf("%d %d\n",x,y);
-			continue;
-		}
-		printf("%d %d\n",x,y);
-		d+=sqrt((x-c)*(x-c)+y*y);
-		y=0;
-		c=x;
-		*/
+		printf("%d:%d-%d\n",n,b,e);
 	}
 	printf("%lf\n",d);
 	return 0;
