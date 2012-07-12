af40ccafa9d521c4828f5cd99921a44a84866ffa
Noriaki <n.pujita@gmail.com>
2012-07-12 23:46:50 +0900

    modify algorithm

--- 001/D/main.rb
+++ 001/D/main.rb
@@ -1,41 +1,53 @@
 #! ruby
 # -*- coding: utf-8 -*-
 
-$ranges = []
-
-def calc_dist r_min, r_max, start, goal
-	dx = goal - start
-	dy = r_max - r_min
-
-	if r_min == r_max
-		return 0
-	elsif r_max - r_min == 1
-		return Math.hypot dx, dy
-	end
-
-	(r_min..r_max).each do |row|
-		rng = $ranges[row]
-		pass = start + dx * (row - r_min) / dy
-		if pass < rng[:l]
-			return calc_dist(r_min, row, start, rng[:l]) +
-				calc_dist(row, r_max, rng[:l], goal)
-		elsif pass > rng[:r]
-			return calc_dist(r_min, row, start, rng[:r]) +
-				calc_dist(row, r_max, rng[:r], goal)
-		end
-	end
-
-	Math.hypot dx, dy
-end
-
 lambda{
+	# データ読み込み
 	n = gets.to_i
 	start, goal = *gets.split(/ /).map{|c| c.to_i}
-	n += 1
-	n.times do
+
+	ranges = Array.new(n+1) do
 		l, r = *gets.split(/ /).map{|c| c.to_i}
-		$ranges << { :l => l, :r => r }
+		{ :l => l, :r => r }
 	end
 
-	puts calc_dist 0, n-1, start, goal
+	# 左右の視野角(簡易)
+	l_view_angle = -Float::MAX
+	r_view_angle = Float::MAX
+	# 左右障害になりうる壁のy座標
+	l_wall_y = r_wall_y = 0
+	
+	dist = 0.0
+	turning_y = 0
+	y = 1
+	while y <= n do
+		rng = ranges[y]
+		l_va = (rng[:l] - start) / (y - turning_y)
+		if l_va >= l_view_angle
+			l_view_angle = l_va
+			l_wall_y = y
+		end
+		r_va = (rng[:r] - start) / (y - turning_y)
+		if r_va <= r_view_angle
+			r_view_angle = r_va
+			r_wall_y = y
+		end
+		if r_view_angle < l_view_angle
+			if y == r_wall_y # 右の壁が奥=左コーナー
+				x = ranges[l_wall_y][:l]
+				dist += Math.hypot(x - start, l_wall_y - turning_y)
+				start, turning_y = x, l_wall_y
+			else # 右コーナー
+				x = ranges[r_wall_y][:r]
+				dist += Math.hypot(x - start, r_wall_y - turning_y)
+				start, turning_y = x, r_wall_y
+			end
+			l_view_angle = -Float::MAX
+			r_view_angle = Float::MAX
+			y = turning_y
+		end
+		y += 1
+	end
+	dist += Math.hypot(goal - start, n - turning_y)
+	puts dist
 }.call
