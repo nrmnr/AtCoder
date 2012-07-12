9510e20a297a66fdf32da012a65d1327a5fc4c9e
n-fujita <n-fujita@daiichi-g.co.jp>
2012-07-12 18:58:53 +0900

    一応完成(WA)

--- 001/D/main.cpp
+++ 001/D/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 struct Range
@@ -13,28 +14,47 @@ typedef vector<Range> Ranges;
 
 Ranges ranges;
 
-double calc_dist(int r_begin, int r_end, int start, int goal)
+double calc_dist(int r_min, int r_max, int start, int goal)
 {
-	for (int row = r_begin; row < r_end; ++row) {
-		
+	const double dx = goal - start;
+	const double dy = r_max - r_min;
+
+	if (r_min == r_max) {
+		return 0;
+	} else if (r_max - r_min == 1) {
+		return sqrt(dx*dx+dy*dy);
 	}
-	return 0;
+
+	for (int row = r_min; row <= r_max; ++row) {
+		const Range rng = ranges[row];
+		const double pass = start + dx * (row - r_min) / dy;
+		if (pass < rng.l) {
+			return
+				calc_dist(r_min, row, start, rng.l) +
+				calc_dist(row, r_max, rng.l, goal);
+		} else if (pass > rng.r) {
+			return
+				calc_dist(r_min, row, start, rng.r) +
+				calc_dist(row, r_max, rng.r, goal);
+		}
+	}
+	return sqrt(dx*dx+dy*dy);
 }
 
 int main()
 {
 	int n, start, goal;
 	cin >> n >> start >> goal;
-	// cout << n << " " << c << " " << g << endl;
 	++n;
+
 	for(;n-->0;){
 		int l, r;
 		cin >> l >> r;
 		ranges.push_back(Range(l,r));
-		cout << n << ":" << l << "-" << r << endl;
 	}
+
 	cout << fixed << setprecision(11)
-			 << calc_dist(0, ranges.size(), start, goal)
+			 << calc_dist(0, ranges.size()-1, start, goal)
 			 << endl;
 	return 0;
 }
