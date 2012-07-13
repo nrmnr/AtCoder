6bb2d0efca5d3984226e54ab50887de95061e97e
n-fujita <n-fujita@daiichi-g.co.jp>
2012-07-13 14:28:23 +0900

    modify algorithm (but MLE)

--- 001/D/main.rb
+++ 001/D/main.rb
@@ -4,24 +4,27 @@
 lambda{
 	# データ読み込み
 	n = gets.to_i
-	start, goal = *gets.split(/ /).map{|c| c.to_i}
+	start, goal = *gets.split(/ /).map{|c| c.to_f}
 
 	ranges = Array.new(n+1) do
-		l, r = *gets.split(/ /).map{|c| c.to_i}
+		l, r = *gets.split(/ /).map{|c| c.to_f}
 		{ :l => l, :r => r }
 	end
+	# Goalの範囲はgoal地点のみとみなす
+	ranges[n][:l] = ranges[n][:r] = goal
 
 	# 左右の視野角(簡易)
 	l_view_angle = -Float::MAX
 	r_view_angle = Float::MAX
 	# 左右障害になりうる壁のy座標
 	l_wall_y = r_wall_y = 0
-	
+
 	dist = 0.0
 	turning_y = 0
 	y = 1
 	while y <= n do
 		rng = ranges[y]
+
 		l_va = (rng[:l] - start) / (y - turning_y)
 		if l_va >= l_view_angle
 			l_view_angle = l_va
@@ -32,19 +35,20 @@ lambda{
 			r_view_angle = r_va
 			r_wall_y = y
 		end
-		if r_view_angle < l_view_angle
-			if y == r_wall_y # 右の壁が奥=左コーナー
+
+		# 左の壁が右の壁の右方にきた
+		if l_view_angle > r_view_angle
+			if y == r_wall_y # 右の壁が奥=左カーブ
 				x = ranges[l_wall_y][:l]
-				dist += Math.hypot(x - start, l_wall_y - turning_y)
-				start, turning_y = x, l_wall_y
-			else # 右コーナー
+				y = l_wall_y
+			else
 				x = ranges[r_wall_y][:r]
-				dist += Math.hypot(x - start, r_wall_y - turning_y)
-				start, turning_y = x, r_wall_y
+				y = r_wall_y
 			end
+			dist += Math.hypot(x - start, y - turning_y)
+			start, turning_y = x, y
 			l_view_angle = -Float::MAX
 			r_view_angle = Float::MAX
-			y = turning_y
 		end
 		y += 1
 	end
