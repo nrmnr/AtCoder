d294ada2bf7cfb15564acb7b17b217a8f651fe0b
n-fujita <n-fujita@daiichi-g.co.jp>
2012-07-13 17:41:14 +0900

    modify 002-C main.c (not complete)

--- 002/C/main.c
+++ 002/C/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 int main()
 {
-	int n;
-	char cmd[1001];
+	int i,n;
 	scanf("%d\n",&n);
+	if(n <= 2){printf("%d\n",1); return 0;}
+	if(n <= 4){printf("%d\n",2); return 0;}
+
+	char cmd[1001];
 	gets(cmd);
 	//printf("%s\n",cmd);
 
