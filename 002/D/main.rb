0143872c2ca6a72dad0e6c12e2a972981a0c302b
Noriaki <n.pujita@gmail.com>
2012-07-15 00:32:37 +0900

    add 002-D rb,data

---
+++ 002/D/main.rb
@@ -0,0 +1,20 @@
+#! ruby
+# -*- coding: utf-8 -*-
+
+lambda {
+	h,w = *gets.split(/ /).map{|c| c.to_i}
+	lines = readlines.map{|l| l.chomp}
+
+	lines.each do |line|
+		odd = even = 0
+		line.scan /(o+)(\.+)(x+)/ do |o,d,x|
+			puts o+d+x
+			if d.size.odd?
+				odd += 1
+			else
+				even += 1
+			end
+		end
+		puts odd, even
+	end
+}.call
