65ab3c887ed9484c75ad97a4ff776884a0aaed29
Noriaki <n.pujita@gmail.com>
2012-07-14 14:45:49 +0900

    add main.rb (AC)

---
+++ 002/C/main.rb
@@ -0,0 +1,26 @@
+#! ruby
+# -*- coding: utf-8 -*-
+
+lambda {
+	short_cut = []
+	for c1 in %w(A B X Y) do
+		for c2 in %w(A B X Y) do
+			short_cut << c1 + c2
+		end
+	end
+	short_cut = short_cut.sort.uniq
+
+	gets
+	cmd = gets.chomp
+
+	len = cmd.size
+	for l in short_cut do
+		for r in short_cut do
+			break if l == r
+			new_len = cmd.gsub(/#{l}/,'L').gsub(/#{r}/,'R').size
+			len = new_len if new_len < len
+		end
+	end
+
+	puts len
+}.call
