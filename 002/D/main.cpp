b4065cb640725dc146ba139b88cd2b1c46d9601b
nrmnr <n.pujita+github@gmail.com>
2012-07-25 06:46:52 +0900

    002ｰD実装

--- 002/D/main.cpp
+++ 002/D/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <cstdin>
+
 using namespace std;
+
+char *board;
+int rows, cols;
+
+void load_board()
+{
+	scanf("%d%d\n", &rows, &cols);
+	board = new char[rows * cols];
+	string line;
+	for (; !getline(cin, line).eof(); ) {
+		for (int i = 0; i < cols; ++i) {
+			
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
+	if (board) {
+    delete [] board;
+	}
 	return 0;
 }
