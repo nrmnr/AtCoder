6bfc7944f8dbd62b0a309953bf1eef24aa06294c
nrmnr <n.pujita+github@gmail.com>
2012-08-04 03:47:41 +0900

    004-C cppに変更

--- 004/C/main.cpp
+++ 004/C/main.cpp
@@ -1,9 +1,35 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+long long gcd(long long a, long long b){
+	long long t;
+	for(; a; t=b, b=a, a=t%a);
+	return b;
+}
 int main()
 {
-	int x, y, m, n;
-	scanf("%d/%d", &x, &y);
-	/* printf("%d %d\n", x, y); */
-	/* puts("Impossible"); */
+	bool f = false;
+	char c;
+	long long x, y, g, z, m, n;
+	cin >> x >> c >> y;
+	g = gcd(x, y);
+	x /= g; y /= g;
+
+	n = 2*x/y;
+	if((2*x)%y != 0) n++;
+
+	z = ((n+1)*y-2*x)*n;
+	if(z % (2*y) == 0){
+		m = z / (2*y);
+		cout << n << " " << m << endl;
+		f = true;
+	}
+	++n;
+	if(2*x == y*(n-1)){
+		cout << n << " " << n << endl;
+		f = true;
+	}
+	if(!f){
+		cout << "Impossible" << endl;
+	}
 	return 0;
 }
